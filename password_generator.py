8da848b0f0f3f49a9ce59fb687b7e3281656f00b
N3squ1K <135476450+0xN3squ1K@users.noreply.github.com>
2026-02-14 13:10:12 -0300

    Update password_generator.py

--- password_generator.py
+++ password_generator.py
@@ -4,15 +4,19 @@ letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","ñ","o","p",
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '@', '#', '$', '%', '^', '&', '*', '+', '=', '(', ')' ]
 
-print("Welcome to the PyPassword Generator!")
-nr_letter = int(input("How many letters? "))
+print('''
+    ###########################################
+    ## Welcome to the Py-Password Generator! ##
+    ###########################################
+    ''')
+nr_letters = int(input("How many letters? "))
 nr_numbers = int(input("How many numbers? "))
-nr_symbols = int(input("How many sumbols? "))
+nr_symbols = int(input("How many symbols? "))
 
-random_letters = random.sample(letters, nr_letter)
+random_letters = random.sample(letters, nr_letters)
 random_numbers = random.sample(numbers, nr_numbers)
 random_symbols = random.sample(symbols, nr_symbols)
 
-password = random_letters + random_numbers + random_symbols
-rand_password = random.sample(password, len(password))
+prev_password = random_letters + random_numbers + random_symbols
+rand_password = random.sample(prev_password, len(prev_password))
 print(f'Your password is: {''.join(rand_password)}')
